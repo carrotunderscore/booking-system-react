584b9b75882a6c9a76fa772bdc189c08dc03f6f2
carrotunderscore <robertpergustafsson@gmail.com>
2021-12-15 16:59:43 +0100

    put footer on all pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import RegisterPage from "./pages/RegisterPage";
 import BookingPage from "./pages/BookingPage";
 import LoginPage from "./pages/LoginPage";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 export default function App() {
@@ -13,12 +14,12 @@ export default function App() {
         <div className="App">
             <Header/>
             <Routes>
-                
                 <Route exact path="/" element={<LoginPage/>}/>
                 <Route exact path="/book" element={<BookingPage/>}/>
                 <Route exact path="/admin" element={<AdminPage/>}/>
                 <Route exact path="/register" element={<RegisterPage/>}/>
             </Routes>
+            <Footer/>
         </div>
     );
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -94,7 +94,7 @@ export default function LoginPage() {
                     </div>
                 </div>
 
-                <Footer/>
+                
             </div>
         </>
     )
