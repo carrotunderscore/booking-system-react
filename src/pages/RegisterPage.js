5438782d3da0d0c707cce492e64560767bd552f5
misterZink <heidarirobin@protonmail.com>
2021-12-21 12:13:32 +0100

    Done, changed so you have to be logged in to reach different areas

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,10 +1,17 @@
 import React from "react";
 import '../../src/index.css';
 import RegisterForm from "../components/registerForm";
+import {Navigate} from "react-router-dom";
+import getEmailFromToken from "../utils/CustomerUtils";
 
 
 
 export default function RegisterPage() {
+    const customerEmail = getEmailFromToken();
+
+    if (customerEmail == null) {
+        return <Navigate to="/"/>
+    }
  
 
     return (
